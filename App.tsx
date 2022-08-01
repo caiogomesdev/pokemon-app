64f007ddc0bbf315fcffd5e277c20da2628965d1
caiogomesdev <caiogame100@gmail.com>
2022-08-01 20:46:25 -0300

    feat: configure to init after load the fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,49 @@
+import { useCallback, useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Roboto_400Regular,
+          Roboto_500Medium,
+          Roboto_700Bold,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container} onLayout={onLayout}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
