a47f8298dc648f2928f257999f1aac0fc73359c9
caiogomesdev <caiogame100@gmail.com>
2022-08-02 12:05:02 -0300

    feat: create Favorites view

---
+++ src/views/favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+interface Params {
+  pokemonIndex: number;
+}
+
+const Favorites: React.FC<Params> = () => {
+  return <Container></Container>;
+};
+
+export default Favorites;
