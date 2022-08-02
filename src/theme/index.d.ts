db7170295a2bb540f845820d432e3c8bdcd6c8d8
caiogomesdev <caiogame100@gmail.com>
2022-08-01 22:05:00 -0300

    chore: configure theme declaration type

---
+++ src/theme/index.d.ts
@@ -0,0 +1,7 @@
+import 'styled-components';
+import { THEME as theme } from './';
+
+declare module 'styled-components' {
+  type ThemeType = typeof theme;
+  export interface DefaultTheme extends ThemeType {}
+}
