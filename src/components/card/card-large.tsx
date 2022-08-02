ed35a376275e18cec832d31c635719dfe2445b8d
caiogomesdev <caiogame100@gmail.com>
2022-08-01 23:14:35 -0300

    feat: add content into card large

--- src/components/card/card-large.tsx
+++ src/components/card/card-large.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { CardContent } from './interfaces';
-import { CardLarge as Card } from './styles';
+import { CardLarge as Card, Description, Title, Image, Column } from './styles';
 
 interface Params {
   content: CardContent;
 }
 
-const CardLarge: React.FC<Params> = () => {
-  return <Card></Card>;
+const CardLarge: React.FC<Params> = ({ content }) => {
+  return (
+    <Card>
+      <Image source={{ uri: content.image }} />
+      <Column>
+        <Title>{content.title}</Title>
+        <Description>{content.description}</Description>
+      </Column>
+    </Card>
+  );
 };
 
 export default CardLarge;
--- src/components/card/styles.ts
+++ src/components/card/styles.ts
@@ -6,6 +6,33 @@ export const CardLarge = styled.View`
   background-color: ${({ theme }) => theme.COLORS.PRIMARY_900};
   width: 100%;
   height: 132px;
+  padding: 20px 39px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 12px;
+  font-family: ${({ theme }) => theme.FONTS.BOLD};
+  color: ${({ theme }) => theme.COLORS.LIGHT_700};
+`;
+
+export const Description = styled.Text`
+  font-size: 10px;
+  margin-top: 5px;
+  font-family: ${({ theme }) => theme.FONTS.REGULAR};
+  color: ${({ theme }) => theme.COLORS.LIGHT_800};
+`;
+
+export const Image = styled.Image`
+  width: 70px;
+  height: 70px;
+`;
+
+export const Column = styled.View`
+  width: 60%;
 `;
 
 export const CardItem = styled.View``;
