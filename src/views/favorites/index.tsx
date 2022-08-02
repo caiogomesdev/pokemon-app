c598e7e6db836efd5c9eff326d6c28d0a5c60ef4
caiogomesdev <caiogame100@gmail.com>
2022-08-02 20:04:14 -0300

    feat: add title and card on favorites

--- src/views/favorites/index.tsx
+++ src/views/favorites/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Title, SafeArea } from './styles';
+import Card from '../../components/card';
+import { CardContent } from '../../components/card/interfaces';
 
 interface Params {
   pokemonIndex: number;
 }
 
 const Favorites: React.FC<Params> = () => {
-  return <Container></Container>;
+  const favorites: CardContent = {
+    id: 1,
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+    title: 'adasdds',
+    description: 'qweqweqw eqwqeqweqwewqqweqeqdas as asdsaad asd adsadczxxzcsc',
+  };
+  return (
+    <SafeArea>
+      <Container>
+        <Title>Favoritos</Title>
+        <Card type="large" showButtonDetails={true} content={favorites} />
+      </Container>
+    </SafeArea>
+  );
 };
 
 export default Favorites;
--- src/views/favorites/styles.ts
+++ src/views/favorites/styles.ts
@@ -1,3 +1,17 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const SafeArea = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  padding: 20px;
+  background-color: ${({ theme }) => theme.COLORS.LIGHT_800};
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-family: ${({ theme }) => theme.FONTS.REGULAR};
+  color: ${({ theme }) => theme.COLORS.PRIMARY_900};
+`;
