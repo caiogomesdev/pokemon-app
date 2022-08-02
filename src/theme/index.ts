fcd47fa6709f2f83cca03412619855b7dc80b73c
caiogomesdev <caiogame100@gmail.com>
2022-08-01 21:45:09 -0300

    feat: define token fonts and colors

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export const THEME = {
+  COLORS: {
+    PRIMARY_900: '#040C19',
+    PRIMARY_800: '#0E1F40',
+    PRIMARY_700: '#162645',
+    PRIMARY_600: '#263C66',
+    SECONDARY_900: '#CC4B00',
+    SECONDARY_800: '#F25D07',
+    SECONDARY_700: '#FF7A2E',
+    LIGHT_900: '#CCC2C2',
+    LIGHT_800: '#F2F2F2',
+    LIGHT_700: '#FFFFFF',
+  },
+  FONTS: {
+    REGULAR: 'Roboto_400Regular',
+    MEDIUM: 'Roboto_500Medium',
+    BOLD: 'Roboto_700Bold',
+  },
+};
