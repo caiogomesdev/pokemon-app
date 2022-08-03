d815a26ccb8cc9d95fa0ee07a04dae6c45e9d85c
caiogomesdev <caiogame100@gmail.com>
2022-08-03 03:26:33 -0300

    feat: add favorite details

--- src/routes/favorites-stack-route.tsx
+++ src/routes/favorites-stack-route.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useTheme } from 'styled-components';
 import { RootStackParamList } from '.';
 
 import Favorites from '../views/favorites';
+import Details from '../views/details';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const FavoritesStackRoute: React.FC = () => {
+  const theme = useTheme();
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="Favorites"
         component={Favorites}
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={Details}
+        options={{
+          headerBackTitle: '',
+          headerTintColor: `${theme.COLORS.PRIMARY_900}`,
+          headerStyle: {
+            backgroundColor: `${theme.COLORS.LIGHT_800}`,
+          },
+        }}
       />
     </Stack.Navigator>
   );
